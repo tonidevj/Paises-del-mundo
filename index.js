bd69ba1534c4c90f1089ce005b5009c38bd029e6
tonidevj <bogadoantoni4@gmail.com>
2024-05-11 02:05:45 -0400

    detallitos finales

--- index.js
+++ index.js
@@ -1,55 +1,120 @@
-const contenedorPaises = document.getElementById('paises-lista');
+const searchInput = document.querySelector('#buscador');
+const container = document.querySelector('#paises-lista');
+const containerNormal = document.querySelector('#paises-information')
+const information = document.querySelector('.info')
+const tarjetInfo = document.querySelector('#add')
 
-document.addEventListener('keyup', e =>{
+// Los paises descargados desde la api se guardan en el array de countries
+// La api deberia pedirse solo una vez
+// Usar este array para crear el filtrado
+let countries = [];
 
-  if(e.target.matches('#buscador')){
-    document.querySelectorAll(".paises-item").forEach( countries =>{
-      countries.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-      ?countries.classList.remove('filtro')
-      :countries.classList.add('filtro')
-    })
-  }
- 
-} )
-
-
-
+// Funcion que pide todos los paises
 const cargarPaises = async () => {
-    try {
-        const respuesta = await fetch('https://restcountries.com/v3.1/all');
 
-        if (respuesta.status === 200) {
-            const datos = await respuesta.json();
+    // Llamo a la API Rest Countries
+    const respuesta = await fetch(`https://restcountries.com/v3.1/all`);
 
-            datos.forEach(pais => {
-                const nombreComun = pais.name.common;
-                const bandera = pais.flags.png;
+     // Transformo la respuesta a JSON
+  const datos = await respuesta.json();
 
-                const paisHTML = `
-                    <li class="paises-item">
-                        <div id="paises-img">
-                            <img class="paises-img-info" src="${bandera}" alt="">
-                        </div>
-                        <div id="paises-info">
-                            <p class="nombre-pais">${nombreComun}</p>
-                        </div>
-                    </li>
-                `;
+  // Guardo el array de los paises recibido dentro de contries
+countries = datos;
+ 
+;}
 
-                contenedorPaises.innerHTML += paisHTML;
-            });
-
-        } else if (respuesta.status === 401) {
-            console.log('Error 401: No autorizado');
-        } else if (respuesta.status === 404) {
-            console.log('Error 404: El país no se encuentra disponible');
-        } else {
-            console.log('Error inesperado');
-        }
-
-    } catch (error) {
-        console.log('Error:', error);
-    }
-}
 
 cargarPaises();
+
+searchInput.addEventListener('input', async e => {
+    const search = e.target.value.toLowerCase();
+    
+    let countriesInfoHTML = '';
+    const paisesFiltro = countries.filter(p => p.name.common.toLowerCase().startsWith(search));
+    information.innerHTML ='';
+    info.innerHTML ='';
+
+    if (paisesFiltro.length === 0){
+        info.innerHTML = `<p class="info">
+                            Especifica mejor tu búsqueda.
+                          </p>`;
+    } else if(paisesFiltro.length > 10){
+        info.innerHTML = `<p class="info">
+                            Demasiados países, especifica mejor tu búsqueda.
+                          </p>`;
+    } else {
+        // Función asincrónica para obtener el clima
+        const obtenerClima = async (lat, lon) => {
+            const climaRespuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=dd0715961b797ee9d38075e60157e745&units=metric`);
+            const climaDatos = await climaRespuesta.json();
+            return climaDatos;
+        };
+
+        // Iterar sobre los países y obtener el clima
+        for (const p of paisesFiltro) {
+            const climaDatos = await obtenerClima(p.latlng[0], p.latlng[1]);    
+            const nube = climaDatos.weather[0].icon;                        
+            const tiempo = climaDatos.main.temp;
+            const nombre = p.name.common;
+            const bandera = p.flags.svg;
+            const capital = p.capital;
+            const continente = p.continents;
+            const time = p.timezones;
+            const habitantes = p.population;
+            countriesInfoHTML += `
+                <li class="paises-item">
+                    <div id="paises-img">
+                        <img class="paises-img-info" src="${bandera}" alt="">
+                    </div>
+                    <div id="paises-info">
+                        <p class="nombre-pais-title">${nombre}</p>
+                        <p class="nombre-pais-info">Capital: ${capital}</p>
+                        <p class="nombre-pais-info">Habitantes: ${habitantes}</p>
+                        <p class="nombre-pais-info">Continente ${continente}</p>
+                        <p class="nombre-pais-info">Zona horaria: ${time}</p>
+                        <div class="tiempo"> 
+                          <img class="nombre-pais-info" src= "https://openweathermap.org/img/wn/${nube}@2x.png">
+                          <p class="nombre-pais-info"> ${tiempo} Celsius</p>
+                        </div>
+                    </div>
+                </li>
+            `;
+        };
+        container.innerHTML = countriesInfoHTML;
+    };
+                                container.addEventListener('click', e => {
+                                    let listItem = e.target.closest('.paises-item');
+                                    const pe = listItem.querySelectorAll('.nombre-pais-info');
+                                    pe.forEach(info => {
+                                        info.classList.remove('nombre-pais-info');
+                                        info.classList.add('nombre-pais');
+                                    });
+                                    // Si se hizo clic en un <li> o algún elemento dentro de él
+                                    if (listItem) {
+                                        listItem.classList.remove('paises-item');
+                                        listItem.classList.toggle('paises-itemenes');
+                                        const ulInfo = document.querySelector('#paises-information');
+                                        ulInfo.appendChild(listItem);
+                                        // Imprime el <li> completo
+                                        // Aquí puedes realizar cualquier acción que necesites con el elemento <li>
+                                    };
+                                });
+                                    // primero cambiar la clase del p
+                                containerNormal.addEventListener('click', e => {
+                                    let listNormal = e.target.closest('.paises-itemenes');
+                                    const normal = listNormal.querySelectorAll('.nombre-pais');
+                                     normal.forEach(noInfo => {
+                                        noInfo.classList.remove('nombre-pais');
+                                        noInfo.classList.add('nombre-pais-info');
+                                    })
+                                    // Si se hizo clic en un <li> o algún elemento dentro de él
+                                    if (listNormal) {
+                                        listNormal.classList.remove('paises-itemenes');
+                                        listNormal.classList.toggle('paises-item');
+                                        const ulNoInfo = document.querySelector('#paises-lista');
+                                        ulNoInfo.appendChild(listNormal);
+                                // Imprime el <li> completo
+                                        // Aquí puedes realizar cualquier acción que necesites con el elemento <li>
+                                    };
+                                });
+});
